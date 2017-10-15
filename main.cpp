d7a74d46c85355ba2b8d8cb34b9246d5a4f04db8
Fitz <f18846188605@gmail.com>
2017-10-15 16:52:39 +0800

    开始写输入数据的内容

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int college_num=20,item_num=20;
 int man_item_num=20;
 int item_data[M][M];
+int choice;
 char item_name[M][M];  //学院的名称  数组前一个表示学院的编号，后一个是字符串长度
-
 typedef struct college_data
 {
     int id;
@@ -18,11 +18,24 @@ typedef struct college_data
     int score_sum,item_man_sum,item_woman_sum;
 }college_data;
 
+void init_menu()
+{
+    cout<<"您想在线输入比赛成绩or从已有文件读取数据？"<<endl;
+    cout<<">>>1.在线输入比赛成绩"<<endl<<">>>2.从已有文件读取数据"<<endl<<"请输入您的选择：(1 or 2)"<<endl;
+    cin>>choice;
+    while(choice<=0||choice>2)
+    {
+        cout<<"超出范围！！！"<<endl<<"请重新输入(1 or 2)"<<endl;
+        cin>>choice;
+    }
+    return;
+}
+
 class university
 {
 public:
     college_data college[M];
-
+    string college_name[M];
     university()
     {
         for(int i=0;i<college_num;i++)
@@ -30,6 +43,7 @@ public:
             college[i].id=i;
             college[i].name="学院";
             college[i].name+=(char)(i+'0');
+            college_name[i]=college[i].name;
             memset(college[i].score,0, sizeof(college[i].score));
         }
         for(int i=0;i<item_num;i++)
@@ -220,7 +234,21 @@ public:
             cout<<"访问的数据超出范围"<<endl;
             return;
         }
-
+        cout<<"您要找的项目"<<item_name[search_item_id]<<"的排名如下"<<endl;
+        if(search_item_id<man_item_num)
+        {
+            for(int i=0;i<3;i++)
+            {
+                cout<<"名次"<<i<<"\t"<<college_name[item_data[search_item_id][i]]<<endl;
+            }
+        }
+        else
+        {
+            for(int i=0;i<5;i++)
+            {
+                cout<<"名次"<<i<<"\t"<<college_name[item_data[search_item_id][i]]<<endl;
+            }
+        }
     }
 private:
     void print_out()
@@ -241,6 +269,27 @@ private:
 };
 
 int main() {
+    choice = 0;
+    init_menu();
+    if(choice==1)
+    {
+        do
+        {
+            cout<<"请输入学院的数量：(请勿大于20)"<<endl;
+            cin>>college_num;
+        }while(college_num>20||college_num<=0);
+        do
+        {
+            cout<<"请输入项目的数量：(请勿大于40)"<<endl;
+            cin>>item_num;
+        }while(item_num>40||item_num<=0);
+        do
+        {
+            cout<<"请输入男生项目的数量：(请勿大于40)"<<endl;
+            cin>>man_item_num;
+        }while(man_item_num>20||man_item_num<=0);
+
+    }
     university hit;
     cout<<hit.college[9].name<<endl;
     cout<<1<<"\t";

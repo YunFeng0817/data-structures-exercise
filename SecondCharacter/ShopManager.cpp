fed8a2d32aa35672563c66a8250ac03f2708dbf3
Fitz <f18846188605@gmail.com>
2017-10-28 01:48:23 +0800

    '完成了链表类的编写‘'

--- SecondCharacter/ShopManager.cpp
+++ SecondCharacter/ShopManager.cpp
@@ -13,7 +13,7 @@ typedef struct cellElement{
 
 class chain{
     cellElement *head=NULL;
-
+    cellElement *previous = head;
     void insert(int id,string n,string b,int price,int a)
     {
         if(head==NULL){
@@ -76,24 +76,87 @@ class chain{
         }
     }
 
-    cellElement loate(int id){
+    cellElement* loate(int id){
         if(head==NULL){
-            cout<<"数据还未录入，无法查询！"<<endl;
-            return;
+            //cout<<"数据还未录入，无法查询！"<<endl;
+            return NULL;   //此处注意意外情况
         }
         else{
-            cellElement *p = head;
+            cellElement *p = previous;
             while (p->next!=NULL){
                 if(p->id==id)
                 {
-                    return *p;
+                    return p;
                 }
+                previous = p;
                 p=p->next;
             }
             if(p->next==NULL){
-                cout<<"找不到此商品！"<<endl;
+                //cout<<"找不到此商品！"<<endl;
+                return NULL;
             }
         }
+    }
+
+    void Delete(int id){
+        cellElement *temp = this->loate(id);
+        if(temp==NULL){
+            return;
+        }
+        else{
+            cellElement *p;
+            p = previous->next;
+            if(p->next==NULL){
+                previous->next=NULL;
+            }
+            else{
+                previous->next=p->next;
+            }
+            free(p);
+        }
+    }
+
+    void modify(int id,int AddAmount){
+        cellElement *p = this->loate(id);
+        if(p==NULL){
+            if(AddAmount<0) {
+                cout<<"该物品没有库存了，无法提货！"<<endl;
+                return;
+            }
+            string a,b;
+            int price;
+            cout<<"请补充该商品的信息"<<endl<<"请输入该商品的名称："<<endl;
+            cin>>a;
+            cout<<"请输入该商品的品牌："<<endl;
+            cin>>b;
+            cout<<"请输入该商品的价格："<<endl;
+            cin>>price;
+            this->insert(id,a,b,price,AddAmount);
+        }
+        if(p->amount+AddAmount<0){
+            cout<<"库存不足，无法提出如此多的货物！"<<endl;
+            return;
+        }
+        p->amount+=AddAmount;
+        cout<<"数据更新成功！"<<endl;
+        return;
+    }
+
+    void empty()
+    {
+        cellElement *p = head;
+        cellElement *temp;
+        while(p->next!=NULL){
+            if(p==head){
+                p = NULL;
+                continue;
+            }
+            temp = p->next;
+            free(p);
+            p = temp;
+        }
+        free(p);
+        return;
     }
 
 };

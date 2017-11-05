36d14549e872d76c310a522135c5bbc145ee4f9d
Fitz <f18846188605@gmail.com>
2017-11-05 12:48:14 +0800

    写完了多项式的乘法，测试完了输入部分，乘法和加法部分没有测试

--- SecondCharacter/PloynomialOperation.cpp
+++ SecondCharacter/PloynomialOperation.cpp
@@ -20,13 +20,13 @@ cellElement * attach(int c,int e,cellElement *d)
 
 cellElement * padd(cellElement *a,cellElement *b)
 {
-    cellElement *p,*q,*d,*c;
+    cellElement *p,*q,*d,*c,*del1,*del2;
     c=new cellElement;
     d=c;
     int temp;
     p=a->link;
-    q=a->link;
-    while((!p->link==NULL)&&(!q->link==NULL))
+    q=b->link;
+    while(!(p->link==NULL)&&!(q->link==NULL))
     {
         if(p->exp==q->exp)
         {
@@ -35,29 +35,42 @@ cellElement * padd(cellElement *a,cellElement *b)
             {
                 d=attach(temp,p->exp,d);
             }
+            del1=p;
+            del2=q;
             p=p->link;
             q=q->link;
+            delete(del1);
+            delete(del2);
         }
         else if(p->exp>q->exp)
         {
             d=attach(p->coef,p->exp,d);
+            del1=p;
             p=p->link;
+            delete(del1);
         }
         else
         {
             d=attach(q->coef,q->exp,d);
+            del2=q;
             q=q->link;
+            delete(del2);
         }
     }
     while(p!=NULL)
     {
         d=attach(p->coef,p->exp,d);
+        del1=p;
         p=p->link;
+        delete(del1);
+
     }
     while(q!=NULL)
     {
         d=attach(q->coef,q->exp,d);
+        del2=q;
         q=q->link;
+        delete(del2);
     }
     d->link=NULL;
     p=c;
@@ -66,8 +79,84 @@ cellElement * padd(cellElement *a,cellElement *b)
     return c;
 }
 
+cellElement * mult(cellElement *a,cellElement *b)
+{
+    cellElement *p,*q,*result1,*result2,*index,*del;
+    int c,e;
+    p=a;
+    q=b;
+    result1=NULL;
+    result2=new cellElement;
+    index=result2;
+    while(p!=NULL)
+    {
+        while(q!=NULL)
+        {
+            cout<<1<<endl;
+            c=p->coef*q->coef;
+            e=p->exp*q->exp;
+            if(c)
+            {
+                index=attach(c,e,index);
+                del=index;
+                q=q->link;
+                delete(del);
+            }
+        }
+        result2=result2->link;
+        if(result1==NULL)
+        {
+            result1=result2;
+        }
+        else
+        {
+            result1=padd(result1,result2);
+        }
+        del=p;
+        p=p->link;
+        delete(del);
+    }
+    return result1;
+}
+
 int main()
 {
-
+    int a,b;
+    cellElement *input1=new cellElement,*input2=new cellElement,*index1,*index2,*result;
+    index1=input1;
+    index2=input2;
+    cout<<"请输入第一个多项式的值：（输入 0 0 时结束第一个多项式的输入）"<<endl;
+    cin>>a;
+    cin>>b;
+    do
+    {
+        index1=attach(a,b,index1);
+        cin>>a;
+        cin>>b;
+    }while(a!=0&&b!=0);
+    input1=input1->link;
+    cout<<"请输入第二个多项式的值：（输入 0 0 时结束第二个多项式的输入）"<<endl;
+    cin>>a>>b;
+    do
+    {
+        index2=attach(a,b,index2);
+        cin>>a;
+        cin>>b;
+    }while(a!=0&&b!=0);
+    input2=input2->link;
+    result=mult(input1,input2);
+    while(result!=NULL)
+    {
+        cout<<result->coef<<"\t"<<result->exp<<endl;
+        result=result->link;
+    }
     return 0;
 }
+
+/*
+1 2
+1 1
+0 0
+1 1
+0 0
+ */

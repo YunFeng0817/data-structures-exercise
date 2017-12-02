b4d062bd59c79642214dcfb0d63558e29f09aab6
Fitz <f18846188605@gmail.com>
2017-12-03 00:27:30 +0800

    完成了深度优先搜索非递归的算法

--- Map/searchMap.cpp
+++ Map/searchMap.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <fstream>
+#include <stack>
 #include <cstdlib>
 using namespace std;
 #define maxNum 30
@@ -48,6 +49,11 @@ void readFile()
     cout<<"请输入存储图信息的文件位置"<<endl<<"tips:格式：顶点个数，边的个数，顶点编号1，顶点编号2，边的权重"<<endl;
     cin>>filePath;
     fileReader.open(filePath.c_str());
+    if(!fileReader.is_open())
+    {
+        cout<<"找不到该文件，请输入有效的文件路径"<<endl;
+        exit(0);
+    }
     fileReader>>p>>e;
     newMap.initMap(p,e);
     for(int i=0;i<e;i++)
@@ -91,10 +97,60 @@ void DFSRecursiveMain()
     }
 }
 
+void DFSNotRecursive()
+{
+    count=0;
+    int index,flag;
+    stack<int> trace;
+    for(int i=0;i<newMap.pointNum;i++)
+    {
+        visited[i]=false;
+        dfsCode[i]=-1;
+    }
+    for(int i=0;i<newMap.pointNum;i++)
+    {
+        index=i;
+        while(!visited[index])
+        {
+            visited[index]=true;
+            dfsCode[count]=index;
+            trace.push(index);
+            count++;
+            do
+            {
+                flag=0;
+                for(int j=0;j<newMap.pointNum;j++)
+                {
+                    if(!visited[j]&&newMap.edge[index][j])
+                    {
+                        index=j;
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0)
+                {
+                    trace.pop();
+                    if(trace.empty())
+                    {
+                        flag=1;
+                        break;
+                    }
+                    else
+                    {
+                        index=trace.top();
+                    }
+                }
+            }while(flag==0);
+        }
+    }
+}
+
 int main()
 {
     readFile();
-    DFSRecursiveMain();
+    //DFSRecursiveMain();
+    DFSNotRecursive();
     for(int i=0;i<newMap.pointNum;i++)
         cout<<dfsCode[i]<<endl;
     return 0;
